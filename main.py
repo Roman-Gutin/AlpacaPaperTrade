b0d85c341f0e3a5dcf697de85b149a3272cc6406
Roman-Gutin <48274547+Roman-Gutin@users.noreply.github.com>
2022-08-09 16:25:44 -0400

    live quotes

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 import streamlit as st
 import pandas as pd
 from feed import QuoteFeed
-tickers = ['SQQQ','TQQQ','QQQ']
+tickers = ['SQQQ']
 feed = QuoteFeed(tickers)
 st.title('LETF Paper Trade Dashboard')
+feed.start()
+
+class QuoteMetric:
+    fields = ['bid_price', 'ask_price']
+    
+    def __init__(self, ticker):
+        self.metrics = dict(zip(self.fields, st.columns(len(fields)))
+        self.ticker = ticker
+        self.last_quote = None
+                            
+        for col_name, column in self.metrics.items():
+            with column:
+                  st.header(self.ticker)
+                  st.metric(col_name, "0", "0")
+                            
+                            
+                            
+     def update(self, quote):
+          for col_name, column in self.metrics.items:
+              with column:
+                   st.header(self.ticker)
+                   st.metric(
+                       label = col_name, 
+                       value = quote[col_name], 
+                       delta = self._delta(quote, col_name)
+                   )
+        
+      def _delta(self,quote, field):
+           delta = (quote[field]-self.last_quote[field])/self.last_quote[field]
+           self.last_quote = quote
+           return f'{delta*100} bps'
+                                                          
+                            
+latest_quotes = {ticker:QuoteMetric(ticker) for ticker in tickers}
+
+while True:
+    for ticker, quote in feed.live_quotes:
+        lastest_quotes[ticker].update(quote)
 
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
-  
-
+                                
