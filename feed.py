3bc71d23a740949c07cf8950dd093c84c7500798
Roman-Gutin <48274547+Roman-Gutin@users.noreply.github.com>
2022-08-09 15:26:33 -0400

    Create feed.py

---
+++ feed.py
@@ -0,0 +1,38 @@
+from concurrent.futures import ThreadPoolExecutor
+from alpaca_trade_api.stream import Stream, URL
+
+ALPACA_KEYS = {
+    'key': 'AK09KE1XGIXEKIOCUWEJ',
+     "secret": 'oKYYBxJ427LPTEHJmjAaW24SdZv9KGejVbn3eqX3' }
+
+ALPACA_API_KEY  , ALPACA_SECRET_KEY  = ALPACA_KEYS.values()
+
+class QuoteFeed:
+    thread_pool = ThreadPoolExecutor(2)
+  
+    def __init__(self,tickers):
+        self.tickers = tickers
+    
+    
+    async def on_quote(self, q):
+        ticker = q.symbol
+        self.live_quotes[ticker] = q
+         
+    def connect(self):
+        self.conn = Stream(ALPACA_API_KEY,
+                      ALPACA_SECRET_KEY,
+                      base_url=URL('https://paper-api.alpaca.markets'),
+                      data_feed='sip')
+
+
+        self.live_quotes = {ticker:None for ticker in self.tickers}
+        self.conn.subscribe_quotes(self.on_quote, *self.tickers) 
+        self.conn.run()
+        
+    def stop(self):
+        self.conn.stop()
+    
+    def start(self):
+        self.thread_pool.submit(self.connect)
+        
+    
